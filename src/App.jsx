0f2cce1d980ba520da7fb0091f25e19c8505d745
halaxysounds <halaxysounds@gmail.com>
2022-04-29 14:18:06 -0300

    remove unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import "./styles/App.css";
-import CreateRoundedIcon from "@mui/icons-material/CreateRounded";
 import { useState, useEffect } from "react";
-import { getNotes } from "./Functions/NoteManager";
-
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import CircularProgress from "@mui/material/CircularProgress";
 
 import { auth } from "./firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
 
-import Note from "./Components/Note";
-import Header from "./Components/Header";
-import SearchBar from "./Components/SearchBar";
-import Create from "./Components/Create";
 import SignIn from "./Screens/SignIn";
-import Account from "./Components/Account";
-import EditNoteModal from "./Components/EditNoteModal";
-import { DataContext } from "./Context/DataContext";
 import Main from "./Screens/Main";
 import { AuthContext } from "./Context/AuthContext";
 
